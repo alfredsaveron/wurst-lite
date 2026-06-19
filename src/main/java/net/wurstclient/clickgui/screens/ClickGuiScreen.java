a23fd82e6cbfc1737ceee6cec6a31b852ba1c20f
alfredsaveron <goldengamersege@gmail.com>
2026-06-19 23:14:41 +0200

    Add Gradle wrapper binaries and scripts

---
+++ src/main/java/net/wurstclient/clickgui/screens/ClickGuiScreen.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014-2026 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.clickgui.screens;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.resources.ResourceLocation;
+import net.wurstclient.clickgui.ClickGui;
+
+public final class ClickGuiScreen extends Screen
+{
+	private static final ResourceLocation WURST_LOGO_TEXTURE =
+		ResourceLocation.fromNamespaceAndPath("wurst", "wurstlite.png");
+	
+	private final ClickGui gui;
+	
+	public ClickGuiScreen(ClickGui gui)
+	{
+		super(net.minecraft.network.chat.Component.literal(""));
+		this.gui = gui;
+	}
+	
+	@Override
+	protected void init()
+	{
+		gui.onOpen(this.width);
+	}
+	
+	@Override
+	public boolean isPauseScreen()
+	{
+		return false;
+	}
+	
+	@Override
+	public void render(GuiGraphics context, int mouseX, int mouseY,
+		float partialTicks)
+	{
+		context.fill(0, 0, this.width, this.height, 0x75000000);
+		gui.render(context, mouseX, mouseY, partialTicks);
+		
+		if(net.wurstclient.WurstClient.INSTANCE.getOtfs().wurstLogoOtf
+			.isShowInGui())
+		{
+			int logoWidth = 115;
+			int logoHeight = 71;
+			int x = this.width - logoWidth - 5;
+			int y = this.height - logoHeight - 5;
+			context.blit(WURST_LOGO_TEXTURE, x, y, logoWidth, logoHeight, 0.0F,
+				0.0F, 574, 356, 574, 356);
+		}
+	}
+	
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int mouseButton)
+	{
+		gui.handleMouseClick((int)mouseX, (int)mouseY, mouseButton);
+		return super.mouseClicked(mouseX, mouseY, mouseButton);
+	}
+	
+	@Override
+	public boolean mouseReleased(double mouseX, double mouseY, int mouseButton)
+	{
+		gui.handleMouseRelease(mouseX, mouseY, mouseButton);
+		return super.mouseReleased(mouseX, mouseY, mouseButton);
+	}
+	
+	@Override
+	public boolean mouseScrolled(double mouseX, double mouseY,
+		double horizontalAmount, double verticalAmount)
+	{
+		gui.handleMouseScroll(mouseX, mouseY, verticalAmount);
+		return super.mouseScrolled(mouseX, mouseY, horizontalAmount,
+			verticalAmount);
+	}
+}
