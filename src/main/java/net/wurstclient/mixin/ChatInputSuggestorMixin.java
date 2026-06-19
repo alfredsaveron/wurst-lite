f9d1945f924530505f874f31b40b9f17763787e7
alfredsaveron <goldengamersege@gmail.com>
2026-06-19 23:15:11 +0200

    Implement mixin for Camera setups

---
+++ src/main/java/net/wurstclient/mixin/ChatInputSuggestorMixin.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014-2026 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.gui.components.CommandSuggestions;
+
+@Mixin(CommandSuggestions.class)
+public abstract class ChatInputSuggestorMixin
+{}
