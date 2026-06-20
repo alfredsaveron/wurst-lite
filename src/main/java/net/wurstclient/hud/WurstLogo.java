cb13493a325a4f6da62e1cc9bb9816950f2a426e
alfredsaveron <goldengamersege@gmail.com>
2026-06-20 14:05:50 +0200

    typo

--- src/main/java/net/wurstclient/hud/WurstLogo.java
+++ src/main/java/net/wurstclient/hud/WurstLogo.java
@@ -41,7 +41,8 @@ public final class WurstLogo
 		context.drawString(tr, version, 74, 8, otf.getTextColor(), false);
 		
 		// Wurst logo
-		context.blit(LOGO_TEXTURE, 0, 3, 72, 18, 0F, 0F, 574F, 356F, 574, 356);
+		context.blit(LOGO_TEXTURE, 0, 3, 72, 18, 0.0F, 0.0F, 574, 356, 574,
+			356);
 	}
 	
 	private String getVersionString()
