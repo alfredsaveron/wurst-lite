d88e0c4744c98410797d428848f7627452bbce48
alfredsaveron <goldengamersege@gmail.com>
2026-08-04 14:06:48 +0300

    clean up WalkPathProcessor

--- src/main/java/net/wurstclient/ai/WalkPathProcessor.java
+++ src/main/java/net/wurstclient/ai/WalkPathProcessor.java
@@ -29,28 +29,18 @@ public class WalkPathProcessor extends PathProcessor
 	@Override
 	public void process()
 	{
-		// get positions
-		BlockPos pos;
-		if(WurstClient.MC.player.onGround())
-			pos = BlockPos.containing(WurstClient.MC.player.getX(),
-				WurstClient.MC.player.getY() + 0.5,
-				WurstClient.MC.player.getZ());
-		else
-			pos = BlockPos.containing(WurstClient.MC.player.position());
+		var player = WurstClient.MC.player;
+		BlockPos pos = player.onGround()
+			? BlockPos.containing(player.getX(), player.getY() + 0.5, player.getZ())
+			: BlockPos.containing(player.position());
 		PathPos nextPos = path.get(index);
 		int posIndex = path.indexOf(pos);
 		
-		if(posIndex == -1)
-			ticksOffPath++;
-		else
-			ticksOffPath = 0;
+		ticksOffPath = posIndex == -1 ? ticksOffPath + 1 : 0;
 		
-		// update index
 		if(pos.equals(nextPos))
 		{
 			index++;
-			
-			// disable when done
 			if(index >= path.size())
 				done = true;
 			return;
@@ -58,17 +48,14 @@ public class WalkPathProcessor extends PathProcessor
 		if(posIndex > index)
 		{
 			index = posIndex + 1;
-			
-			// disable when done
 			if(index >= path.size())
 				done = true;
 			return;
 		}
 		
 		lockControls();
-		WurstClient.MC.player.getAbilities().flying = false;
+		player.getAbilities().flying = false;
 		
-		// face next position
 		facePosition(nextPos);
 		if(Mth.wrapDegrees(Math.abs(RotationUtils
 			.getHorizontalAngleToLookVec(Vec3.atCenterOf(nextPos)))) > 90)
@@ -76,67 +63,42 @@ public class WalkPathProcessor extends PathProcessor
 		
 		if(WURST.getHax().jesusHack.isEnabled())
 		{
-			// wait for Jesus to swim up
-			if(WurstClient.MC.player.getY() < nextPos.getY()
-				&& (WurstClient.MC.player.isInWater()
-					|| WurstClient.MC.player.isInLava()))
+			if(player.getY() < nextPos.getY() && (player.isInWater() || player.isInLava()))
 				return;
 			
-			// manually swim down if using Jesus
-			if(WurstClient.MC.player.getY() - nextPos.getY() > 0.5
-				&& (WurstClient.MC.player.isInWater()
-					|| WurstClient.MC.player.isInLava()
-					|| WURST.getHax().jesusHack.isOverLiquid()))
+			if(player.getY() - nextPos.getY() > 0.5 && (player.isInWater() || player.isInLava() || WURST.getHax().jesusHack.isOverLiquid()))
 				MC.options.keyShift.setDown(true);
 		}
 		
-		// horizontal movement
 		if(pos.getX() != nextPos.getX() || pos.getZ() != nextPos.getZ())
 		{
 			MC.options.keyUp.setDown(true);
-			
-			if(index > 0 && path.get(index - 1).isJumping()
-				|| pos.getY() < nextPos.getY())
+			if(index > 0 && path.get(index - 1).isJumping() || pos.getY() < nextPos.getY())
 				MC.options.keyJump.setDown(true);
-			
-			// vertical movement
 		}else if(pos.getY() != nextPos.getY())
-			// go up
+		{
 			if(pos.getY() < nextPos.getY())
 			{
-				// climb up
-				// TODO: Spider
 				Block block = BlockUtils.getBlock(pos);
 				if(block instanceof LadderBlock || block instanceof VineBlock)
 				{
 					WURST.getRotationFaker().faceVectorClientIgnorePitch(
 						BlockUtils.getBoundingBox(pos).getCenter());
-					
 					MC.options.keyUp.setDown(true);
-					
 				}else
 				{
-					// directional jump
-					if(index < path.size() - 1
-						&& !nextPos.above().equals(path.get(index + 1)))
+					if(index < path.size() - 1 && !nextPos.above().equals(path.get(index + 1)))
 						index++;
-					
-					// jump up
 					MC.options.keyJump.setDown(true);
 				}
-				
-				// go down
 			}else
 			{
-				// skip mid-air nodes and go straight to the bottom
-				while(index < path.size() - 1
-					&& path.get(index).below().equals(path.get(index + 1)))
+				while(index < path.size() - 1 && path.get(index).below().equals(path.get(index + 1)))
 					index++;
-				
-				// walk off the edge
-				if(WurstClient.MC.player.onGround())
+				if(player.onGround())
 					MC.options.keyUp.setDown(true);
 			}
+		}
 	}
 	
 	@Override
