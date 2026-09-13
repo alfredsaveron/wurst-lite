6dbaa7283bcabf0db2d2955d79fe893adffdc8a3
alfredsaveron <goldengamersege@gmail.com>
2026-09-13 16:39:57 +0300

    spacing fixes

--- src/main/java/net/wurstclient/mixin/GameMenuScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/GameMenuScreenMixin.java
@@ -63,8 +63,8 @@ public abstract class GameMenuScreenMixin extends Screen
 		if(!WurstClient.INSTANCE.isEnabled() || wurstOptionsButton == null)
 			return;
 		
-		int w = 38;
-		int h = 24;
+		int w = 32;
+		int h = 20;
 		int x =
 			wurstOptionsButton.getX() + (wurstOptionsButton.getWidth() - w) / 2;
 		int y = wurstOptionsButton.getY()
@@ -82,7 +82,7 @@ public abstract class GameMenuScreenMixin extends Screen
 		int buttonX = width / 2 - 102;
 		int buttonY = 60;
 		int buttonWidth = 204;
-		int buttonHeight = 28;
+		int buttonHeight = 20;
 		
 		for(AbstractWidget button : buttons)
 		{
@@ -94,10 +94,10 @@ public abstract class GameMenuScreenMixin extends Screen
 				break;
 			}
 			
-			// If options button exists, go 32px above it
+			// If options button exists, go 24px above it
 			if(isTrKey(button, "menu.options"))
 			{
-				buttonY = button.getY() - 32;
+				buttonY = button.getY() - 24;
 				break;
 			}
 		}
@@ -146,9 +146,9 @@ public abstract class GameMenuScreenMixin extends Screen
 			return;
 		
 		// If yes, clear space below and move the buttons there
-		ensureSpaceAvailable(x, y + 32, width, height);
+		ensureSpaceAvailable(x, y + 24, width, height);
 		for(AbstractWidget button : buttonsInTheWay)
-			button.setY(button.getY() + 32);
+			button.setY(button.getY() + 24);
 	}
 	
 	@Unique
